7bb3081afe92d5ce1d62e676f839d0d603f8a458
jml74262 <jml74262@lasallebajio.edu.mx>
2023-11-26 14:27:19 -0600

    CRUD materias

--- router.js
+++ router.js
@@ -5,27 +5,51 @@ const conexion = require('./database/db');
 
 router.get('/', (req, res) => {
     res.render('index');
-    // conexion.query('SELECT * FROM user', (error, results) => {
-    //     if (error) {
-    //         console.error(error);
-    //         return;
-    //     }else{
-    //         res.send(results)
-    //     }
-    // });
 });
 
 router.get('/materia', (req, res) => {
-    res.render('materia/materia');
+    conexion.query('SELECT * FROM asignature', (error, results) => {
+        if (error) {
+            console.error(error);
+            return;
+        }else{
+            res.render('materia/materia', {materias: results});
+        }
+    });
+    // res.render('materia/materia');
 });
 
 router.get('/create_materia', (req, res) => {
+    
     res.render('materia/create_materia');
 });
 
 //se tiene que agregar :id para que funcione la ruta al final de edit_materia/
-router.get('edit_materia/', (req, res) => {
-    res.render('materia/edit_materia');
+router.get('/edit_materia/:id', (req, res) => {
+    const id = req.params.id;
+    conexion.query('SELECT * FROM asignature WHERE id = ?', [id], (error, results) => {
+        if (error) {
+            console.error(error);
+            return;
+        }else{
+            console.log(results[0]);
+            res.render('materia/edit_materia', {materia: results[0]});
+        }
+    });
+    // res.render('materia/edit_materia');
+});
+
+//ruta para borrar materia
+router.get('/delete_materia/:id', (req, res) => {
+    const id = req.params.id;
+    conexion.query('DELETE FROM asignature WHERE id = ?', [id], (error, results) => {
+        if (error) {
+            console.error(error);
+            return;
+        }else{
+            res.redirect('/materia');
+        }
+    });
 });
 
 router.get('/alumno', (req, res) => {
@@ -39,5 +63,10 @@ router.get('/create_alumno', (req, res) => {
 router.get('/edit_alumno', (req, res) => {
     res.render('alumno/edit_alumno');
 });
+
+//Control de CRUD
+const crud = require('./controllers/crud');
+router.post('/save_materia', crud.save_materia);
+router.post('/update_materia', crud.update_materia);
 
 module.exports = router;
